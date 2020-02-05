e81a7482ab7d13b4325fd36eaab796d451b54e5c
FloresEFederico <60672910+FloresEFederico@users.noreply.github.com>
2020-02-05 11:35:42 -0300

    Ejercicio 10actualizado
    Chequearlo

---
+++ jsEntradaSalida-10.js
@@ -0,0 +1,19 @@
+/*Debemos lograr tomar el importe por ID.
+Transformarlo a entero (parseInt), luego
+mostrar el importe con un Descuento del 25 %
+en el cuadro de texto "RESULTADO"*/
+function mostrarAumento()
+{
+	var importe;
+	var porcentaje;
+	var	resultado;
+
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
+	
+	porcentaje=(25*importe)/100;
+
+	resultado=importe-porcentaje;
+	document.getElementById('resultado').value=resultado;
+
+}
