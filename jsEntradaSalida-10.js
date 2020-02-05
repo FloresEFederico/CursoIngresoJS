f451d8f2b2ab8a5d079b7a801eabc10e56271ca0
FloresEFederico <60672910+FloresEFederico@users.noreply.github.com>
2020-02-05 12:09:44 -0300

    ejer 10 actu
    chequear

--- jsEntradaSalida-10.js
+++ jsEntradaSalida-10.js
@@ -5,6 +5,24 @@ en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
 	var importe;
+	var descuento;
+	var resultado;
+
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
+	descuento=importe-importe*descuento/100;
+
+	resultado=importe-descuento;
+
+	document.getElementById('resultado').value=resultado;
+	
+}
+//precio con descuento= precio- precio * por desc/100  //
+//precio por impuestos= precio + precio * aumento/100  //
+
+//ejercicio 10//
+
+	var importe;
 	var porcentaje;
 	var	resultado;
 
@@ -15,7 +33,3 @@ function mostrarAumento()
 
 	resultado=importe-porcentaje;
 	document.getElementById('resultado').value=resultado;
-
-}
-//precio con descuento= precio- precio * por desc/100//
-//precio por impuestos= precio + precio x aumento/100//
