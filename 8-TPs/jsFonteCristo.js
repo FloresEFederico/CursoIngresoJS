520beaad8ed342d609c9d79f8cd04601444e3dd5
Federico <fedeflores_93@hotmail.com>
2020-02-29 18:54:30 -0300

    2902

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -2,12 +2,137 @@
 
 13.	Para cada una de estas acciones  debemos realizar la lógica para verificar las cualidades pedidas:
 a.	Se pedirán un número positivo y se mostrará la cantidad de números pares desde el número ingresado hasta el cero.
-b.	Se pedirán un número positivo y se mostrará la cantidad de números pares desde el número ingresado hasta el cero.
+b.	Se pedirán un número positivo y se mostrará la cantidad de números impares desde el número ingresado hasta el cero.
 c.	Se pedirán un número positivo y se mostrará la cantidad de números divisibles de este número que se encuentran desde el 1 al 100.
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
+
+function NumerosPares ()
 {
- 	
+	var numeroIngresado;
+	var i;
+	var numPares;
+
+	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+ 	numPares=0;
+
+	for(i=0;i<=numeroIngresado;i++)
+	{
+		if(i%2==0)
+		{
+			numPares++;
+		}
+	}
+	alert(numPares);
+}
+
+function NumerosImpares()
+{
+	var numeroIngresado;
+	var i;
+	var numImpares;
+
+	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+ 	numImpares=0;
+
+	for(i=0;i<=numeroIngresado;i++)
+	{
+		if(i%2!=0)
+		{
+			numImpares++;
+		}
+	}	
+	alert(numImpares);
+
+
+}
+
+function NumerosDivisibles()
+{
+	var numeroIngresado;
+	var i;
+	var divisor;
+	divisor=0;
+
+	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+
+	while(numeroIngresado<1||numeroIngresado>100)
+ 	{
+ 		numeroIngresado=prompt("numero invalido! tiene que ser del 1 al 100");	
+ 	}
+	 	
+	 	for(i=1;i<=numeroIngresado;i++)
+	 	{
+	 		if(numeroIngresado%i==0)
+	 		{
+	 			divisor++;
+	 		}
+	 	}
+
+	 alert(divisor);
+}
+
+function VerificarPrimo()
+{	
+	var numeroIngresado;
+	var i;
+	var numPrimo;
+	numPrimo=0;
+
+	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+
+	for(i=1;i<=numeroIngresado;i++)
+	 	{
+	 		if(numeroIngresado%i==0)
+	 		{
+	 			numPrimo++;
+	 		}
+	 	}
+	 	if(numPrimo==2)
+	 	{
+	 		alert("El numero ingresado es primo.");
+	 	}else
+	 	{
+	 		alert("No es un numero primo.");
+	 	}
+
+}
+function NumerosPrimos()
+{
+	var numeroIngresado;
+	var i;
+	var numPrimoCantidad;
+	numPrimoCantidad=0;
+	var x;
+	var contDivisor;
+	contDivisor=0;
+
+	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+
+	 for(i=0 ; i<=numeroIngresado ; i++)
+	 {
+	 	//ANALIZO SI EL NUMERO ES PRIMO Y LO CUENTO
+	 	for( x=1 ; x<=i ; x++)
+	 	{
+	 		if(i%x==0)
+	 		{
+	 			contDivisor++;
+	 		}
+	 	}
+	 	if(contDivisor==2)
+	 	{
+	 		numPrimoCantidad++;
+	 	}
+	 	//FIN DEL ANALISIS DEL NUMERO
+	 	contDivisor=0;
+	 }
+
+	 alert(numPrimoCantidad);
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,81 @@
 function mostrar()
 {
+	var marca;
+	var peso;
+	var temperatura;
+	var cantPares;
+	var marcaPesado;
+	var canTemp;
+	var promedioPeso;
+	var sumaDePesos;
+	var maximo;
+	var minimo;
+	var contador;
+	var respuesta;
 
+	cantPares=0;
+	canTemp=0;
+	contador=0;
+	maximo=0;
+	minimo=0;
+	sumaDePesos=0;
+	respuesta="si";
+
+	while(respuesta=="si")
+	{	
+		contador++;
+		marca=prompt("Por favor, ingrese una marca");
+
+		peso=prompt("Por favor, ingrese su peso (debe ser entre 1 y 100");
+		while(isNaN(peso)||peso<1 || peso>100)
+		{
+			peso=prompt("peso Invalido! Tiene que ser un numero entre 1 y 100!");
+
+		}
+		peso=parseInt(peso);
+		sumaDePesos=sumaDePesos+peso;
+
+		temperatura=prompt("Ingrese la temperatura de almacenamiento (solo puede ser entre -30 y 30)");
+
+		while(isNaN(temperatura)||temperatura<-30||temperatura>30)
+		{
+			temperatura=prompt("ingreso invalido! entre -30 y 30");
+		}
+		temperatura=parseInt(temperatura);	
+
+		if(temperatura%2==0)
+		{
+			cantPares++;
+		}
+		if(contador==1)
+		{
+			maximo=peso;
+			minimo=peso;
+			marcaPesado=marca;
+		}else
+		{
+			if(peso>maximo)
+			{
+				maximo=peso;
+				marcaPesado=marca;
+			}
+			if(peso<minimo)
+			{
+				minimo=peso;				
+			}		
+		}
+
+		if(temperatura<0)
+		{
+			canTemp++;
+		}
+		respuesta=prompt("seguir? si/no");		
+	}
+	promedioPeso=sumaDePesos/contador;
+
+	document.write("Cantidad de numeros pares "+cantPares+" <br>");
+	document.write("Marca del producto mas pesa'o "+marcaPesado+" <br> ");
+	document.write("cantidad de productos que se conservan a menos de cero grados "+canTemp+" <br>");
+	document.write("promedio del peso de todos los productos "+promedioPeso+" <br> ");
+	document.write("el peso maximo "+maximo+" y el minimo "+minimo+" <br>");
 }
